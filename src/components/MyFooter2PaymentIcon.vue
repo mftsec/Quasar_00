081584bf814a66dd9135d3d5e272267630515b33
ardamerttarkan <arda.tarkan60@hotmail.com>
2023-12-13 15:57:49 +0300

    add footer icon

---
+++ src/components/MyFooter2PaymentIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="payment-icon">
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 64 32"
+    >
+      <text x="50%" y="50%" font-size="12" text-anchor="middle" dy=".3em">
+        Örnek Icon
+      </text>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyFooter2PaymentIcon',
+};
+</script>
+
+<style scoped>
+.payment-icon {
+  color: white;
+}
+</style>
