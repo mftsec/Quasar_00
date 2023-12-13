260d588b06758538382539f70feb006fb8843fdb
ardamerttarkan <arda.tarkan60@hotmail.com>
2023-12-13 14:26:52 +0300

    add searchbox

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-item
+    class="header-search header-search-dropdown has-icon has-dropdown menu-item-has-children"
+  >
+    <q-item-section>
+      <q-icon name="search" class="icon-search" />
+    </q-item-section>
+    <q-item-section>
+      <q-menu>
+        <q-item>
+          <q-input
+            v-model="searchQuery"
+            outlined
+            dense
+            placeholder="Ara…"
+            class="search-field mb-0"
+            @input="search"
+          />
+          <q-icon
+            name="search"
+            class="icon-search ux-search-submit"
+            @click="submitSearch"
+          />
+        </q-item>
+      </q-menu>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const searchQuery = ref('');
+
+const search = () => {
+  // Arama işlemleri
+};
+
+const submitSearch = () => {
+  // Arama formunu gönderme işlemleri
+};
+</script>
+
+<style scoped></style>
