de58f4da2a24126cfd7728581634f278967e0e24
ardamerttarkan <arda.tarkan60@hotmail.com>
2023-12-13 14:20:37 +0300

    add logo

---
+++ src/components/TheLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-logo">
+    <img
+      width="110"
+      height="68"
+      src="https://www.kzkulaklik.com/wp-content/uploads/2021/08/KZ-logo-kzkulaklik.com_.png"
+      alt="my icon"
+    />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.main-logo {
+  margin-right: 12%;
+}
+</style>
