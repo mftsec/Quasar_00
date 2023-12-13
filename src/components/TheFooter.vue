e5869d61abe691b1cec3ec14fced9ebdf01a82a8
ardamerttarkan <arda.tarkan60@hotmail.com>
2023-12-13 14:19:38 +0300

    add footer comp

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-footer class="bg-white text-primary">
+    <q-toolbar class="row items-center justify-between">
+      <MyFooterImage />
+      <MyFooterText />
+    </q-toolbar>
+  </q-footer>
+</template>
+
+<script>
+import MyFooterImage from './MyFooterImage.vue';
+import MyFooterText from './MyFooterText.vue';
+
+export default {
+  name: 'MyFooter',
+  components: {
+    MyFooterImage,
+    MyFooterText,
+  },
+};
+</script>
+
+<style scoped>
+.q-footer {
+  background-color: #ffffff;
+  text-align: center;
+}
+</style>
